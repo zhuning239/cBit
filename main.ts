553761ac403f53a004e8b9969524bc593a1bcc9b
zhuning239 <364711106@qq.com>
2018-03-31 17:04:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -968,13 +968,13 @@ namespace cbit_小车类 {
                     if (value == enLineState.White) {
                         temp = true;
                     }
-                    setPwm(7, 0, 4095);
+                    setPwm(6, 0, 4095);
                 }
                 else {
                     if (value == enLineState.Black) {
                         temp = true;
                     }
-                    setPwm(7, 0, 0);
+                    setPwm(6, 0, 0);
                 }
                 break;
             }
@@ -984,13 +984,13 @@ namespace cbit_小车类 {
                     if (value == enLineState.White) {
                         temp = true;
                     }
-                    setPwm(6, 0, 4095);
+                    setPwm(7, 0, 4095);
                 }
                 else {
                     if (value == enLineState.Black) {
                         temp = true;
                     }
-                    setPwm(6, 0, 0);
+                    setPwm(7, 0, 0);
                 }
                 break;
             }
